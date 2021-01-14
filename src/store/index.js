5ffc82cded7b12ea9d5054197295d56dc220fdc2
AmoDinho <u14284783@tuks.co.za>
2021-01-14 22:36:19 +0200

    created store index

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import { applyMiddleware, createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import { createBrowserHistory } from "history";
+import { initialState } from "../reducers/initialState";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import rootReducer from "../reducers";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+export const history = createBrowserHistory();
+
+const persistConfig = {
+  key: "root",
+  stateReconciler: autoMergeLevel2,
+  storage,
+  whitelist: [""],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer(history));
+
+const middleware = [thunk, reduxImmutableStateInvariant({ ignore: [] })];
+
+export const store = createStore(
+  persistedReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
+
+export const persistor = persistStore(store);
