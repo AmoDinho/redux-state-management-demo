352a32549c8cf74b0c375da3006f7e55710f3132
AmoDinho <u14284783@tuks.co.za>
2021-01-14 23:09:10 +0200

    created layouts reducer

---
+++ src/reducers/layoutReducer.js
@@ -0,0 +1,13 @@
+import * as types from "../actions/actionTypes";
+import initalState from "./initialState";
+
+export const layoutsReducer = (state = initalState.pageHeader, action) => {
+  let s = { ...state };
+  switch (action.type) {
+    case types.SET_PAGE_HEADER:
+      s.pageHeader = { ...action.result };
+      return s;
+    default:
+      return state;
+  }
+};
