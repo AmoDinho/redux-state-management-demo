b4cae1db2b94049f85240682ea3d5ecb40c97f80
AmoDinho <u14284783@tuks.co.za>
2021-03-02 21:21:47 +0200

    it is working

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "@reach/router";
 import Home from "./pages/index";
+import SecondPage from "./pages/SecondPage";
 const Routes = ({ props }) => {
   return (
     <Router>
       <Home path="/" props={props} />
+      <SecondPage path="/second" props={props} />
     </Router>
   );
 };
