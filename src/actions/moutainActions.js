b4458f72f5def80fd91658549caf20a03bb84daf
AmoDinho <u14284783@tuks.co.za>
2021-03-02 21:27:48 +0200

    finished function

--- src/actions/moutainActions.js
+++ src/actions/moutainActions.js
@@ -1,3 +1,14 @@
 import * as types from "./actionTypes";
 
-export const addNewMountain = (mountain) => {};
+export const addNewMountain = (mountain) => {
+  const dispatchResult = (dispatch) => {
+    dispatch({
+      type: types.ADD_NEW_MOUNTAIN,
+      result: {
+        mountain,
+      },
+    });
+  };
+
+  return dispatchResult;
+};
