5c416ddd66b8630035b0f83889fbc7247079b008
AmoDinho <u14284783@tuks.co.za>
2021-03-02 21:44:56 +0200

    it is semi working

--- src/actions/moutainActions.js
+++ src/actions/moutainActions.js
@@ -4,9 +4,7 @@ export const addNewMountain = (mountain) => {
   const dispatchResult = (dispatch) => {
     dispatch({
       type: types.ADD_NEW_MOUNTAIN,
-      result: {
-        mountain,
-      },
+      result: mountain,
     });
   };
 
--- src/reducers/mountainReducer.js
+++ src/reducers/mountainReducer.js
@@ -2,12 +2,11 @@ import * as types from "../actions/actionTypes";
 import initalState from "./initialState";
 
 export const mountainReducer = (state = initalState.mountains, action) => {
-  let s = { ...state };
+  let s = [...state];
   switch (action.type) {
     case types.ADD_NEW_MOUNTAIN:
-      let copyArray = [...s.mountains];
-      copyArray.push(action.result);
-      s.mountains = copyArray;
+      s.push(action.result);
+      s.mountains = s;
       return s;
     default:
       return state;
