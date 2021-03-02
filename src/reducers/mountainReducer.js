49981fa080c967d40f49fa0b7f0503ef27ebd450
AmoDinho <u14284783@tuks.co.za>
2021-03-02 21:34:43 +0200

    added another one

--- src/reducers/mountainReducer.js
+++ src/reducers/mountainReducer.js
@@ -1,4 +1,15 @@
 import * as types from "../actions/actionTypes";
 import initalState from "./initialState";
 
-export const mountainReducer = () => {};
+export const mountainReducer = (state = initalState.mountains, action) => {
+  let s = { ...state };
+  switch (action.type) {
+    case types.ADD_NEW_MOUNTAIN:
+      let copyArray = [...s.mountains];
+      copyArray.push(action.result);
+      s.mountains = copyArray;
+      return s;
+    default:
+      return state;
+  }
+};
